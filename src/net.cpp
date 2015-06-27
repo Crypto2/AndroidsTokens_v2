ce133cd582b2dc2728056ce7da2bb35420daece6
MrData <mrdata@crypto2.net>
2015-06-26 23:20:17 -0400

    Add DNS seed

--- src/net.cpp
+++ src/net.cpp
@@ -1149,6 +1149,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
+    {"adt.dnsseed.crypto2.net", "adt.dnsseed.crypto2.net"},
     {"explorer.androidstokensv2.org", "seed.androidstokensv2.org"},
 };
 
